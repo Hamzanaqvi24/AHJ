161e1fafecab59dcfc1590339f5c0578fd1ba390
amp239 <amp239@njit.edu>
2023-05-01 22:30:24 -0400

    Notif

--- phplogin/fetch.php
+++ phplogin/fetch.php
@@ -1,39 +1,10 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "notif");
-if(isset($_POST['view'])){
-// $con = mysqli_connect("localhost", "root", "", "notif");
-if($_POST["view"] != '')
+if(isset($_POST["subject"]))
 {
-   $update_query = "UPDATE comments SET comment_status = 1 WHERE comment_status=0";
-   mysqli_query($con, $update_query);
-}
-$query = "SELECT * FROM comments ORDER BY comment_id DESC LIMIT 5";
-$result = mysqli_query($con, $query);
-$output = '';
-if(mysqli_num_rows($result) > 0)
-{
-while($row = mysqli_fetch_array($result))
-{
-  $output .= '
-  <li>
-  <a href="#">
-  <strong>'.$row["comment_subject"].'</strong><br />
-  <small><em>'.$row["comment_text"].'</em></small>
-  </a>
-  </li>
-  ';
-}
-}
-else{
-    $output .= '<li><a href="#" class="text-bold text-italic">No Noti Found</a></li>';
-}
-$status_query = "SELECT * FROM comments WHERE comment_status=0";
-$result_query = mysqli_query($con, $status_query);
-$count = mysqli_num_rows($result_query);
-$data = array(
-   'notification' => $output,
-   'unseen_notification'  => $count
-);
-echo json_encode($data);
+$con = mysqli_connect("localhost", "root", "", "ffootball");
+$subject = mysqli_real_escape_string($con, $_POST["subject"]);
+$comment = mysqli_real_escape_string($con, $_POST["comment"]);
+$query = "INSERT INTO comments(comment_subject, comment_text)VALUES ('$subject', '$comment')";
+mysqli_query($con, $query);
 }
 ?>
--- phplogin/insert.php
+++ phplogin/insert.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST["subject"]))
 {
-$con = mysqli_connect("localhost", "root", "", "notif");
+$con = mysqli_connect("localhost", "root", "", "ffootball");
 $subject = mysqli_real_escape_string($con, $_POST["subject"]);
 $comment = mysqli_real_escape_string($con, $_POST["comment"]);
 $query = "INSERT INTO comments(comment_subject, comment_text)VALUES ('$subject', '$comment')";
